2b901b12c11c2b1bb65cdacf8d17353a9bb978d7
mkravy <michaelkravy@gmail.com>
2021-08-16 20:06:24 +0700

    Все работает, осталось поработать с ООП

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace Excel2DB
 {
     class Program
     {
+        private static string query;
+
         static void exceltocsv(string path, string pathcsv, char del)
         {
             //Создаем COM объект
@@ -16,7 +18,7 @@ namespace Excel2DB
             //Проверяем существование Excel
             if (excelApp == null)
             {
-                Console.WriteLine(DateTime.Now.ToLongTimeString() + "\t Excel не установлен");
+                log("Excel не установлен");
                 return;
             }
 
@@ -35,24 +37,30 @@ namespace Excel2DB
                 File.Delete(pathcsv);
                 string label = "Код" + del + "Наименование" + del + "Подразделение" + del + "Владелец процесса" + del + Environment.NewLine;
                 File.WriteAllText(pathcsv, label, Encoding.GetEncoding(1251));
+
+                log("Создан файл " + pathcsv);
             }
             else
             {
                 string label = "Код" + del + "Наименование" + del + "Подразделение" + del + "Владелец процесса" + del + Environment.NewLine;
                 File.WriteAllText(pathcsv, label, Encoding.GetEncoding(1251));
+                Console.WriteLine("Создан файл " + pathcsv);
             }
+
+            log("Начало чтения файла " + path);
+
+            //Через upstring идет сбор строки для *.csv
+            string upstring = "";
 
             //Чтение из файла
             for (int i = 5; i <= rows; i++)
             {
-                //Через upstring идет сбор строки для *.csv
-                string upstring = "";
                 for (int j = 1; j <= cols; j++)
                 {
                     //Проверяем непустые ячейки
                     if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
                     {
-                        //Добавление в строку данных, в пустую(if) и непустую (else)
+                        //Добавление в строку данных
                         upstring = upstring + excelRange.Cells[i, j].Value2.ToString() + del;
                     }
                 }
@@ -63,8 +71,9 @@ namespace Excel2DB
                     excelRange.Cells[i + 1, 2].Value2.ToString();
                     upstring = upstring + del + Environment.NewLine;
                     File.AppendAllText(pathcsv, upstring, Encoding.GetEncoding(1251));
-                    Console.WriteLine(DateTime.Now.ToLongTimeString() + "\t Строка добавлена в файл " + pathcsv);
                     upstring = "";
+
+                    //log("Строка добавлена в файл " + pathcsv);
                 }
                 catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException) { }
 
@@ -72,46 +81,108 @@ namespace Excel2DB
             //Выход из Excel
             excelApp.Quit();
             System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
-            Console.WriteLine(DateTime.Now.ToLongTimeString() + "\t Запись в файл окончена");
+
+            log("Запись в файл " + pathcsv + " окончена");
         }
 
-        static void db_structure()
+        static void db_structure(string db, string table1, string table2, string table3)
         {
-            //Отдельно прописываем название БД, т.к. изначально ее нет, позже будет создана
-            string db = "test", table1 = "main", table2 = "process", table3 = "owners";
-
             //Объект для установления соединения с БД
             string conString = connectionString();
             MySqlConnection connection = new MySqlConnection(conString);
 
-            //Открываем соединение
-            connection.Open();
+            try
+            {
+                //Открываем соединение
+                connection.Open();
+
+                log("Соединение с БД '" + db + "' установлено");
+            }
+            catch (Exception ex)
+            {
+                log_ex(ex, "\t Соединение с БД '" + db + "' не установлено");
+            }
 
             //Запросы
-            //Дропаем таблицу (для теста)
-            string query = "DROP DATABASE IF EXISTS " + db;
-            auto_query(query, connection);
+            try
+            {
+                //Дропаем таблицу (для теста)
+                query = "DROP DATABASE IF EXISTS " + db;
+                auto_query(query, connection);
+            }
+            catch { }
 
-            //Создали базу 'test', если не существует
-            query = "CREATE DATABASE IF NOT EXISTS " + db;
-            auto_query(query, connection);
+            try
+            {
+                //Создали базу 'test', если не существует
+                query = "CREATE DATABASE IF NOT EXISTS " + db;
+                auto_query(query, connection);
+
+                log("Создана база '" + db + "'");
+            }
+            catch (Exception ex)
+            {
+                log_query(ex, "\t База данных '" + db + "' не создана", query);
+            }
 
             //Создание таблиц в 'test'
-            //Таблица id - Название процесса
-            query = "CREATE TABLE " + db + "." + table1 + "(id INT AUTO_INCREMENT, name TEXT, PRIMARY KEY (id))";
-            auto_query(query, connection);
+            try
+            {
+                //Таблица 1: id - Название процесса
+                query = "CREATE TABLE " + db + "." + table1 + "(id INT AUTO_INCREMENT, name TEXT, PRIMARY KEY (id))";
+                auto_query(query, connection);
 
-            //Таблица id = Код процесса
-            query = "CREATE TABLE " + db + "." + table2 + "process (id INT AUTO_INCREMENT, process_code TEXT, PRIMARY KEY(id))";
-            auto_query(query, connection);
+                log("Создана таблица " + table1);
+            }
+            catch (Exception ex)
+            {
+                log_query(ex, "\t Таблица " + table1 + " не создана", query);
+            }
 
-            //Таблица id - подразделения и владельцы
-            query = "CREATE TABLE " + db + "." + table3 + "owners (id INT AUTO_INCREMENT, retail BOOL, vip BOOL, oper BOOL, compliance BOOL, credit BOOL, corp BOOL, pro BOOL, mboard BOOL, sccenter BOOL, PRIMARY KEY(id))";
-            auto_query(query, connection);
+            try
+            {
+                //Таблица 2: id = Код процесса
+                query = "CREATE TABLE " + db + "." + table2 + "(id INT AUTO_INCREMENT, process_code TEXT, PRIMARY KEY(id))";
+                auto_query(query, connection);
+
+                log("Создана таблица " + table2);
+            }
+            catch (Exception ex)
+            {
+                log_query(ex, "\t Таблица " + table1 + " не создана", query);
+            }
+
+            try
+            {
+                //Таблица 3: id - подразделения и владельцы
+                query = "CREATE TABLE " + db + "." + table3 + "(id INT AUTO_INCREMENT, retail BOOL, vip BOOL, oper BOOL, compliance BOOL, credit BOOL, corp BOOL, pro BOOL, mboard BOOL, sccenter BOOL, PRIMARY KEY(id))";
+                auto_query(query, connection);
+
+                log("Создана таблица " + table3);
+            }
+            catch (Exception ex)
+            {
+                log_query(ex, "\t tаблица " + table1 + " не создана", query);
+            }
         }
 
-        static void db_insert(string pathcsv, char del)
+        static void db_insert(string pathcsv, char del, string db, string table1, string table2, string table3)
         {
+            string conString = connectionString();
+            MySqlConnection connection = new MySqlConnection(conString);
+
+            try
+            {
+                //Открываем соединение
+                connection.Open();
+
+                log("Соединение с БД '" + db + "' установлено");
+            }
+            catch (Exception ex)
+            {
+                log_ex(ex, "\t Соединение с БД '" + db + "' не установлено");
+            }
+
             //Добавление строк
             using (StreamReader sr = new StreamReader(pathcsv, System.Text.Encoding.Default))
             {
@@ -192,30 +263,63 @@ namespace Excel2DB
                                                 break;
 
                                             case "St":
-                                                query_insert_owner = query_insert_owner + "scc, ";
+                                                query_insert_owner = query_insert_owner + "sccenter, ";
                                                 query_insert_owner_values = query_insert_owner_values + "1, ";
                                                 break;
                                         }
                                         break;
                                 }
                     }
+
+                    //Формируем запрос
                     try
                     {
-                        //Формируем запрос
-                        string query = "INSERT ";
-                        Console.WriteLine(query_insert_owner + "\t" + query_insert_owner_values.Remove(query_insert_owner_values.Length - 2));
+                        //Добавление наименования процесса в таблицу 1
+                        string query = "INSERT " + db + "." + table1 + "(name) VALUES ('" + process_name + "')";
+                        auto_query(query, connection);                      
                     }
-                    catch { }
+                    catch (Exception ex)
+                    {
+                        log_query(ex, "\t Запрос INSERT в таблицу '" + table1 + "' не добавлен", query);
+                    }
+
+                    try
+                    {
+                        //Добавление кода процесса в таблицу 2
+                        string query = "INSERT " + db + "." + table2 + "(process_code) VALUES ('" + process_code + "')";
+                        auto_query(query, connection);
+                    }
+                    catch(Exception ex)
+                    {
+                        log_query(ex, "\t Запрос INSERT в таблицу '" + table2 + "' не добавлен", query);
+                    }
+
+                    try
+                    {
+                        //Добавление владельцев и подразделения в таблицу 3
+                        //Remove нужен, чтобы убрать последнюю запятую в запросе: (1,1,1,)
+                        string query = "INSERT " + db + "." + table3 + "(" + query_insert_owner.Remove(query_insert_owner.Length-2) + ") VALUES (" + query_insert_owner_values.Remove(query_insert_owner_values.Length - 2) + ")";
+                        auto_query(query, connection);
+                    }
+                    catch(System.ArgumentOutOfRangeException)
+                    {
+                        //Ошибка из-за того, что в эту таблицу нечего записать из соответствующей строки
+                    }
+                    catch(Exception ex)
+                    {
+                        log_query(ex, "\t Запрос INSERT в таблицу '" + table3 + "' не добавлен", query);
+                    }
                 }
             }
         }
+
         static string connectionString()
         {
             string server = "localhost";
             string user = "user";
             string pword = "test";
             string SslMode = "none";
-            string bd = "test";
+            string db = "test";
 
             //Строка подключения к базе данных
             string connectionString = "server=" + server + ";user=" + user + ";password=" + pword + ";SslMode=" + SslMode;
@@ -224,25 +328,71 @@ namespace Excel2DB
 
         static void auto_query(string query, MySqlConnection connection)
         {
-            //Выполнение всех запросов
-            MySqlCommand com = new MySqlCommand(query, connection);
-            com.ExecuteNonQuery();
+            try
+            {
+                //Выполнение всех запросов
+                MySqlCommand com = new MySqlCommand(query, connection);
+                com.ExecuteNonQuery();
+                //log("Запрос " + query + " выполнен");
+            }
+            catch (Exception ex)
+            {
+                //log_ex(ex, "\t Запрос " + query + " не выполнен");
+            }
         }
 
-        
+        static void log(string log_text)
+        {
+            string log_file = "C:\\Users\\micha\\Desktop\\Энергосбыт\\log.txt";
+            string log_string = DateTime.Now.ToLongTimeString() + " " + log_text + "\n";
+            Console.Write(log_string);
+            File.AppendAllText(log_file, log_string, Encoding.GetEncoding(1251));
+        }
 
+        static void log_ex(Exception ex, string log_text)
+        {
+            //Выводит ошибку
+            string log_file = "C:\\Users\\micha\\Desktop\\Энергосбыт\\log.txt";
+            string log_string = "\n" + DateTime.Now.ToLongTimeString() + " " + log_text + "\n \t Ошибка: " + ex + "\n\n";
+            Console.Write(log_string);
+            File.AppendAllText(log_file, log_string, Encoding.GetEncoding(1251));
+        }
+
+
+        static void log_query(Exception ex, string log_text, string query)
+        {
+            //Кроме ошибки выводит сам запрос
+            string log_file = "C:\\Users\\micha\\Desktop\\Энергосбыт\\log.txt";
+            string log_string = "\n" + DateTime.Now.ToLongTimeString() + " " + log_text + "\n \t Ошибка: " + ex + "\n \t Запрос: " + query + "\n\n";
+            Console.Write(log_string);
+            File.AppendAllText(log_file, log_string, Encoding.GetEncoding(1251));
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(DateTime.Now.ToLongTimeString() + "\t Программа запущена");
-            string temp = Environment.CurrentDirectory;
-            string path = @"C:\Users\micha\Desktop\Энергосбыт\тестовые данные.xlsx";
-            string pathcsv = "C:\\Users\\micha\\Desktop\\Энергосбыт\\тест.csv";
+            string log_file = "C:\\Users\\micha\\Desktop\\Энергосбыт\\log.txt";
+            if (File.Exists(log_file))
+            {
+                File.Delete(log_file);
+            }
+            else
+            {
+                log("Программа запущена");
+            }
+
+            //Переменные директив и файлов
+            string temp = Environment.CurrentDirectory, path = @"C:\Users\micha\Desktop\Энергосбыт\тестовые данные.xlsx", pathcsv = "C:\\Users\\micha\\Desktop\\Энергосбыт\\тест.csv";  
+
+            //Разделитель строки в *.csv
             char del = ';';
 
-            //exceltocsv(path, pathcsv, del);
-            //csvtodb(pathcsv, del);
+            //Отдельно прописываем название БД, т.к. изначально ее нет, позже будет создана
+            string db = "test", table1 = "main", table2 = "process", table3 = "owners";
 
-            Console.WriteLine(DateTime.Now.ToLongTimeString() + "\t Работа программы окончена");
+            exceltocsv(path, pathcsv, del);
+            db_structure(db, table1, table2, table3);
+            db_insert(pathcsv, del, db, table1, table2, table3);
+
+            log("Работа программы окончена");
             Console.ReadLine();
         }
     }
